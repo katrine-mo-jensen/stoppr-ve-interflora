2e3acb8d5fb5817118b6e113653bd88facedb186
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2022-11-18 13:29:55 +0100

    jsr

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let large = document.getElementById("large");
+let small = document.getElementsByClassName("small");
+
+for (let index = 0; index < small.length; index++) {
+  small[index].addEventListener("click", (event) => {
+    console.log(large.src);
+
+    console.log(event.target.src);
+
+    large.src = event.target.src;
+  });
+}
