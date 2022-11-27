71888d6fa6d59ec5fe2ce901e49dced7e5491894
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2022-11-27 18:22:10 +0100

    close enough

--- js/app.js
+++ js/app.js
@@ -12,3 +12,20 @@ for (let index = 0; index < small.length; index++) {
   });
 }
 // Accordion
+const btns = document.querySelectorAll(".faq-list button");
+
+for (let index = 0; index < btns.length; index++) {
+  btns[index].addEventListener("click", (e) => {
+    let div = e.target.parentNode.parentNode;
+    div.classList.toggle("active");
+  });
+}
+
+// Contact
+function validate(event) {
+  const form = document.forms["contact"];
+
+  if (form[0].value != "" && form[0].value.length < 10) {
+    alert("Fejl");
+  }
+}
